2c34ac7af59ca5a057698e285c143a30727fbc99
kihuni <stephenkihuni55@gmail.com>
2024-05-22 15:54:16 +0300

    added yaml file

--- project_management/project_management/settings.py
+++ project_management/project_management/settings.py
@@ -24,8 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 DEBUG = os.getenv('DJANGO_DEBUG') == 'True'
-SECRET_KEY = 'django-insecure-4dry1*_zu*cj0**w#1=$iv4_vmpwc==60^l@oz7)&xol@2+58b'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
